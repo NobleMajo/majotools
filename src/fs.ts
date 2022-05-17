b693b51ba06df5f7966cfb695e591f598d16bfe0
majo418 <majo@coreunit.net>
2022-05-17 22:26:38 +0000

    Change: install pathsfilter and add path functions

--- src/fs.ts
+++ src/fs.ts
@@ -3,6 +3,158 @@ import * as path from "path"
 import * as os from "os"
 import { VarInputStream, VarStream } from "./varstream"
 import { JsonTypes } from "./json"
+import {
+    absolutPath,
+    matchPathSelector,
+    PathSelector,
+    toLinuxPath
+} from "pathsfilter"
+
+export interface PathStat {
+    name: string,
+    relativePath: string,
+    rootPath: string,
+    isFile: boolean,
+    isDir: boolean,
+}
+
+export interface RecursivePathStatChildren {
+    [name: string]: RecursivePathStat
+}
+
+export interface RecursivePathStat extends PathStat {
+    children: RecursivePathStatChildren
+    parent: RecursivePathStat | undefined
+}
+
+export interface ExtendedDirScanOptions {
+    depth?: number
+    cwd?: string
+    pathSelector?: PathSelector,
+    matchByDefault?: boolean
+    current?: RecursivePathStat,
+    parent?: RecursivePathStat
+}
+
+export interface ExtendedDirScanSettings {
+    depth: number
+    cwd: string
+    pathSelector: PathSelector | undefined,
+    matchByDefault: boolean
+    current: RecursivePathStat | undefined,
+    parent: RecursivePathStat | undefined
+}
+
+export const defaultExtendedDirScanSettings: ExtendedDirScanSettings = {
+    depth: 5,
+    cwd: process.cwd(),
+    pathSelector: undefined,
+    matchByDefault: false,
+    current: undefined,
+    parent: undefined
+}
+
+export async function extendedDirScan(
+    path: string,
+    options?: ExtendedDirScanOptions
+): Promise<RecursivePathStat> {
+    const settings: ExtendedDirScanSettings = {
+        ...defaultExtendedDirScanSettings,
+        ...options,
+    }
+    path = toLinuxPath(path)
+    let current: RecursivePathStat
+    if (!settings.current) {
+        const stat = await fs.stat(path)
+        current = {
+            name: path.split("/").pop() as string,
+            rootPath: absolutPath(path, settings.cwd),
+            relativePath: path,
+            isFile: stat.isFile(),
+            isDir: stat.isDirectory(),
+            children: {},
+            parent: settings.parent
+        }
+    } else {
+        current = settings.current
+    }
+    if (settings.parent) {
+        settings.parent.children[current.name] = current
+    }
+    if (current.isDir && settings.depth > 0) {
+        const files = await fs.readdir(
+            path,
+            {
+                encoding: "utf-8",
+                withFileTypes: true,
+            }
+        )
+        await Promise.all(files.map(
+            async (file) => {
+                const childPath = path + "/" + file.name
+                if (settings.pathSelector) {
+                    if (
+                        !matchPathSelector(
+                            childPath,
+                            settings.pathSelector,
+                            settings.matchByDefault
+                        )
+                    ) {
+                        return
+                    }
+                }
+                const child: RecursivePathStat = {
+                    name: file.name,
+                    rootPath: absolutPath(childPath, settings.cwd),
+                    relativePath: childPath,
+                    isFile: file.isFile(),
+                    isDir: file.isDirectory(),
+                    children: {},
+                    parent: settings.current,
+                }
+                await extendedDirScan(
+                    childPath,
+                    {
+                        ...settings,
+                        depth: settings.depth - 1,
+                        current: child,
+                        parent: current
+                    }
+                )
+            }
+        ))
+    }
+    return current
+}
+
+export function parseRecursiveFilelist(
+    recursivePathStat: RecursivePathStat,
+): string[] {
+    const result: string[] = []
+    if (recursivePathStat.isFile) {
+        result.push(recursivePathStat.relativePath)
+    } else if (recursivePathStat.isDir) {
+        Object.values(recursivePathStat.children).forEach(
+            (v) => parseRecursiveFilelist(v).forEach(
+                (v) => result.push(v)
+            )
+        )
+    }
+    return result
+}
+
+export function parseAbsoluteRecursiveFilelist(
+    recursivePathStat: RecursivePathStat,
+    cwd: string = process.cwd()
+): string[] {
+    return parseRecursiveFilelist(recursivePathStat).map(
+        (v) => absolutPath(
+            v,
+            cwd
+        )
+    )
+
+}
 
 export function toAbsolutePath(
     filePath: string,
